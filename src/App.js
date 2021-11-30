8eaa64f69555606585843cc7c68b76f4f3543665
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-11-30 11:40:16 +0100

    refactor: move content of App to Header component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
-import React, { useEffect } from 'react';
 import './App.scss';
-import nightSky from './assets/images/nightsky3_sky.jpg';
-import trees from './assets/images/nightsky_only_trees.png';
-import titleRocket from './assets/images/title-rocket-outline.svg';
-import bigRocketIcon from './assets/images/big-rocket-icon-outline.svg';
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
+import Header from './components/Header/Header';
 
 const App = () => {
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to(".big-rocket-icon", {
-      scrollTrigger: {
-        trigger: ".background",
-        start: "top top",
-        endTrigger: ".big-rocket-icon",
-        end: "bottom top",
-        scrub: 1,
-      },
-      x: '99.9vw',
-    });
-    gsap.to(".title-quote", {
-      scrollTrigger: {
-        trigger: ".background",
-        start: "top top",
-        endTrigger: ".big-rocket-icon",
-        end: "top top",
-        scrub: true,
-      },
-      opacity: 0,
-    });
-  }, []);
-
   return (
     <div className="App">
-      <div className="background">
-        <div className="title-rocket-div">
-          <img src={titleRocket} className="title-quote" alt="quote with a rocket icon" />
-          <img src={bigRocketIcon} className="big-rocket-icon" alt="big icon of a rocket" />
-        </div>
-        <img src={trees} className="trees" alt="silhouette of trees in front of the night sky" />
-        <img src={nightSky} className="nightsky" alt="a moving night sky as the website background" />
-      </div>
+      <Header />
     </div>
   );
 }
