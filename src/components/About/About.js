aafa8c0ce97d347ec914e91b80bfd41198d63b82
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-12-02 13:22:03 +0100

    build: add About component

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import './About.scss';
+import houseSky from '../../assets/images/house_nightsky.jpg';
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+const About = () => {
+    
+};
+
+export default About;
