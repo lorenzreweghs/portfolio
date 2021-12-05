8efd0802b60ac0c49bd5785485b8159d2429e51b
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-12-05 15:09:12 +0100

    feat(App): render About component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import About from './components/About/About';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <About />
       <div id="aboutme" className="house-bg"></div>
     </div>
   );
