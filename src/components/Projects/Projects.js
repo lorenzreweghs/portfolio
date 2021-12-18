b6c5d8cce25ecc81dbd62c81fa448b96d60e8433
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-12-18 14:48:29 +0100

    feat(Projects): add projects-list & carousel

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import './Projects.scss';
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import JSON from '../../assets/projects.json';
+import Project from './Project/Project';
+import Carousel from 'react-elastic-carousel';
 
 const Projects = () => {
     useEffect(() => {
@@ -19,11 +22,27 @@ const Projects = () => {
             duration: 0.5,
             ease: "power2.inOut",
         });
+
+        gsap.to(".carousel", {
+            scrollTrigger: {
+                trigger: ".carousel",
+                toggleActions: "restart none none reset",
+                start: "top bottom",
+                end: "bottom top",
+            },
+            opacity: 1,
+            duration: 1
+        })
     }, []);
 
     return (
         <section className="Projects" id="projects">
             <span className="projects-tag">Projects</span>
+            <div className="projects-list">
+                <Carousel className="carousel">
+                    {JSON.projects.map((project) => <Project key={project.name} name={project.name} image={project.image} description={project.description} />)}   
+                </Carousel>         
+            </div>
         </section>
     );
 }
